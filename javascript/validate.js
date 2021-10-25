b84ecfc079f3d5eeb6312fafe43fcf0720b99c18
stutisaurya <stuti.saurya@gmail.com>
2021-10-25 11:18:08 +0530

    Added:Ability to save the Employee Payroll Object to Local Storage.

--- javascript/validate.js
+++ javascript/validate.js
@@ -25,6 +25,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
  const save = () => {
     try {
        let employeePayrollData = createEmployeePayroll();
+       createAndUpdateStorage(employeePayrollData);
     } catch (e) {
        return;
     }
@@ -64,6 +65,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
  const getInputValueById = ( id ) => {
     let value = document.querySelector(id).value;
     return value;
+ }
+ 
+ function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse( localStorage.getItem ("EmployeePayrollList"));
+    if(employeePayrollList != undefined) {
+       employeePayrollList.push(employeePayrollData);
+    } else {
+       employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
  }
  
  
